f4fdd0b87c0c9d5e2ae8fcba673b0e90b170769c
GustavoHenrique <binarymaster100@gmail.com>
2023-05-02 12:09:47 -0300

    chore: Preparação para o deploy

--- src/app.js
+++ src/app.js
@@ -2,19 +2,21 @@ import express from "express"
 import usersRouter from "./routes/signin.routes.js"
 import productsRouter from "./routes/products.routes.js"
 import cors from "cors"
+import dotenv from "dotenv"
 
 //Declaração do servidor
 const app = express()
 
 //Configurações do servidor
+dotenv.config()
 app.use(cors())
 app.use(express.json())
 app.use(usersRouter)
 app.use(productsRouter)
 
 //Configure sua porta se preferir
-const PORT_SERVER = 5000
+const port = process.env.PORT || 5000
 
-app.listen(PORT_SERVER, () => {
-    console.log("Servidor rodando na porta: " + PORT_SERVER)
+app.listen(port, () => {
+    console.log("Servidor rodando na porta: " + port)
 })
